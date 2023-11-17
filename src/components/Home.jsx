55f16969f09374e48afa3d887bd83fe49c8c7e51
Emmanuel <tochukwu01.okpala@gmail.com>
2023-11-17 15:48:42 +0100

    react changes in home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet-async";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../styles/home.css";
 import profile from "../assets/working.png";
 import { useEffect } from "react";
@@ -7,14 +7,13 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 
 const Home = () => {
-  const goToRepos = useNavigate();
 
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
 
   return (
-      <div className="home">
+    <div className="home">
       <Helmet>
         <title>Home</title>
         <meta
@@ -23,40 +22,34 @@ const Home = () => {
         />
         <link rel="canonical" href="/" />
       </Helmet>
-        <div className="home-text">
-          <h1 data-aos="fade-up" data-aos-once="true">
-            Hello my name is Joseph
-          </h1>
-          <p data-aos="fade-up" data-aos-once="true" data-aos-delay="100">
-            This is a project for the third semester exam in AltSchool Africa.
-            In this project I am going to be making a request from the GitHub
-            API to be able to display a list of all my repositories and other
-            information. I would also implement a wildcard route for error
-            pages.
-          </p>
-          <Link to="/repos" data-aos="fade-up" data-aos-once="true" data-aos-delay="150">Go to Repositories</Link>
-        </div>
-        <div class="working-man">
-          <img
-            src={profile}
-            alt="man working on laptop"
-            data-aos="fade-up"
-            data-aos-once="true"
-            data-aos-delay="200"
-          />
-        </div>
+      <div className="home-text">
+        <h1 data-aos="fade-up" data-aos-once="true">
+          Hello my name is Joseph
+        </h1>
+        <p data-aos="fade-up" data-aos-once="true" data-aos-delay="100">
+          This is a project for the third semester exam in AltSchool Africa. In
+          this project I am going to be making a request from the GitHub API to
+          be able to display a list of all my repositories and other
+          information. I would also implement a wildcard route for error pages.
+        </p>
+        <Link
+          to="/repos"
+          data-aos="fade-up"
+          data-aos-once="true"
+          data-aos-delay="150"
+        >
+          Go to Repositories
+        </Link>
       </div>
-
-      // <div>
-      //   <button
-      //     onClick={() => goToRepos("/repos")}
-      //     data-aos="fade-up"
-      //     data-aos-once="true"
-      //     data-aos-delay="150"
-      //   >
-      //     Repos
-      //   </button>
-      // </div>
+      <img
+        className="working-man"
+        src={profile}
+        alt="man working on laptop"
+        data-aos="fade-up"
+        data-aos-once="true"
+        data-aos-delay="200"
+      />
+    </div>
   );
 };
 
